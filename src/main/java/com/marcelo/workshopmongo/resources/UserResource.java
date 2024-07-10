a546a287676c132861d787d2baf372877d6cf9ba
Marcelo <msr88957@gmail.com>
2024-07-10 20:54:19 -0300

    return response entity

---
+++ src/main/java/com/marcelo/workshopmongo/resources/UserResource.java
@@ -0,0 +1,35 @@
+package com.marcelo.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcelo.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	
+	@GetMapping
+	public ResponseEntity<List<User>> fundAll(){
+		
+		User maria = new User("1", "Maria silva", "maria@gmail.com");
+		
+		User alex = new User("1", "Alex silva", "alex@gmail.com");
+		
+		List<User> list = new ArrayList<>();
+		
+		list.addAll(Arrays.asList(maria, alex));
+		
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	
+}
