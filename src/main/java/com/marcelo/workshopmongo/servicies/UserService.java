d3f68302e936bbbffe2af926e4d92ae6064ce8a2
Marcelo <msr88957@gmail.com>
2024-07-10 22:20:48 -0300

    find user by id

--- src/main/java/com/marcelo/workshopmongo/servicies/UserService.java
+++ src/main/java/com/marcelo/workshopmongo/servicies/UserService.java
@@ -1,12 +1,15 @@
 package com.marcelo.workshopmongo.servicies;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcelo.workshopmongo.domain.User;
 import com.marcelo.workshopmongo.repository.UserRepository;
+import com.marcelo.workshopmongo.servicies.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -20,4 +23,27 @@ public class UserService {
 		
 	}
 
+	public User findById(String id) {
+		
+		
+		try {
+			Optional<User> userData = repository.findById(id);
+			
+			User user = userData.get();
+			
+			if(user == null) {
+				
+				throw new ObjectNotFoundException("Usuário não encontrado");
+				
+			}
+			
+			return user;
+		}catch(NoSuchElementException e) {
+			throw new ObjectNotFoundException("Usuário não encontrado");
+
+		}
+
+		
+	}
+	
 }
